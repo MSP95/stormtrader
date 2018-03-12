0c4c49028a3b51971c3f9510ed23e76977d2df89
Varad Choudhari <varad.choudhari@gmail.com>
2018-03-12 11:53:56 -0400

    Implemented BUY modules

--- assets/js/game.jsx
+++ assets/js/game.jsx
@@ -13,6 +13,13 @@ class Game extends React.Component {
     let playerStatus = "";
     this.playerStatus = this.getPlayerStatus();
     this.channel = props.channel;
+    this.state = {
+      stocks_names: ["APPL", "GOOG", "FB"],
+      stocks_price: [100, 200, 300],
+      stocks_qty: [50, 50, 50],
+      player1: {wallet: "", own: []},
+      player2: {wallet: "", own: []},
+    }
   }
   getPlayerStatus() {
     if (current_user == this.props.users[this.props.users.length - 1]) {
@@ -47,7 +54,7 @@ class Game extends React.Component {
       </div>
       <div className="padding"></div>
       <div className="grid">
-        <Trade />
+        <Trade stocksNames={this.state.stocks_names} stocksPrice={this.state.stocks_price} stocksQty={this.state.stocks_qty}/>
         <StocksDB />
         <OtherPlayer />
         <News />
@@ -68,7 +75,7 @@ class Timer extends React.Component {
   }
 
   componentDidMount() {
-      this.channel.on('start_timer', (response) => {
+    this.channel.on('start_timer', (response) => {
       //console.log(response.time);
       let minutes = Math.floor(response.time / 60);
       let seconds = response.time - minutes * 60;
@@ -82,10 +89,84 @@ class Timer extends React.Component {
   }
 }
 
-function Trade() {
-  return(<div className="trade">
-  <h5>Trade</h5>
-</div>);
+class Trade extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buyStock = this.buyStock.bind(this);
+    this.handleStockName = this.handleStockName.bind(this);
+    this.handleStockQty = this.handleStockQty.bind(this);
+    this.state = {
+      input_mismatch_error: "",
+      stock_incart: "",
+      symbols: [],
+      stock_qty_incart: "",
+      cart_total: "",
+      stock_current_price: "",
+    }
+  }
+  
+  buyStock(event) {
+    event.preventDefault();
+    let stock_name = event.target.stock_name.value;
+    let stock_quantity = event.target.stock_quantity.value;
+    this.setState({input_mismatch_error: "You cannot buy this"})
+  }
+
+  handleStockName(event) {
+    let input = event.target.value;
+    let stocksNames = this.props.stocksNames;
+    let stocksPrice = this.props.stocksPrice;
+    let index = stocksNames.indexOf(input);
+    if (stocksNames.includes(input.toUpperCase())) {
+      this.setState({stock_incart: input.toUpperCase(), symbols: ["x"], stock_current_price: "($"+stocksPrice[index]+")", input_mismatch_error: ""})
+    }
+    else if (input === "") {
+      this.setState({stock_qty_incart: "", cart_total: "", stock_incart: "", symbols: [], stock_current_price: "", input_mismatch_error: ""})
+    }
+    else {
+      this.setState({stock_qty_incart: "", cart_total: "", stock_incart: "", symbols: [], stock_current_price: "", input_mismatch_error: "Invalid Stock Name"})
+    }
+  }
+
+  handleStockQty(event) {
+    let input = event.target.value;
+    let stock_incart = this.state.stock_incart;
+    let stocksQty = this.props.stocksQty;
+    let stocksPrice = this.props.stocksPrice;
+    let stocksNames = this.props.stocksNames;
+    let index = stocksNames.indexOf(stock_incart);
+    if (input <= stocksQty[index]) {
+      let price = input * stocksPrice[index]
+      this.setState({stock_qty_incart: input, cart_total: "$"+price, symbols: ["x","="], input_mismatch_error: ""})
+    }
+    else {
+      this.setState({stock_qty_incart: "", cart_total: "", symbols: [], input_mismatch_error: "Demand exceeding the available quantity"})
+    }
+
+  }
+
+
+  render() {
+    return(<div className="trade">
+    <div className="trade-grid">
+      <div className="trade-operations"></div>
+      <div className="trade-operations">
+        <form onSubmit={this.buyStock}>
+          <div className="input-group">
+            <input onChange={this.handleStockName} className="form-control" name="stock_name" type="text" placeholder="Stock Name"></input>
+            <input onChange={this.handleStockQty} className="form-control" name="stock_quantity" type="number" placeholder="Quantity"></input>
+          </div>
+          <div className="height-p4em"></div>
+          <input className="buy-btn btn-success" type="submit" value="BUY"></input>
+        </form>
+      </div>
+      <div className="buy-error"><p>{this.state.input_mismatch_error}</p></div>
+      <div className="buy-invoice">
+        <h5><span className="width-p5em">{this.state.stock_incart}</span><span className="width-p5em">{this.state.stock_current_price}</span><span className="width-p5em">{this.state.symbols[0]}</span><span className="width-p5em">{this.state.stock_qty_incart}</span><span className="width-p5em">{this.state.symbols[1]}</span><span className="width-p5em">{this.state.cart_total}</span></h5>
+      </div>
+    </div>
+  </div>);
+}
 }
 
 function StocksDB() {
