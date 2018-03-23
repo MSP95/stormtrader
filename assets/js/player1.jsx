b60b9c653bcd460a10c4ab148848439a266e69b8
Varad Choudhari <varad.choudhari@gmail.com>
2018-03-23 18:27:05 -0400

    Added Spectator Modules

---
+++ assets/js/player1.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Player1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sum: 0,
+      own: [],
+    }
+  }
+
+  getCurrentWorth(stocks_price, own) {
+    let sum = 0
+    for(let i = 0; i < own.length; i++) {
+      let owned = own[i]
+      sum = sum + owned["qty"] * stocks_price[owned.stock_id]
+    }
+    this.setState({sum: sum})
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({own: newProps.player.own})
+    this.getCurrentWorth(newProps.stocksPrice, newProps.player.own)
+  }
+
+  render() {
+    return(<div className="account">
+    <div className="header">
+      <h5>Stocks You Own</h5>
+    </div>
+    <div className="subheader">Current Market Worth : {this.state.sum}</div>
+    <div className="account-block">
+      <table className="account-table">
+        <tbody>
+        <tr><td className="tdspace">Name</td><td className="tdspace">Qty.</td><td className="tdspace">Price</td></tr>
+        {this.state.own.map((data) => {
+          return(<tr key={data.id}><td className="tdspace">{data.stock_name}</td><td className="tdspace">{data.qty}</td><td className="tdspace">{data.bought_at}</td></tr>)
+        })}
+      </tbody>
+      </table>
+    </div>
+  </div>)
+}
+}
--- assets/js/spectate.jsx
+++ assets/js/spectate.jsx
@@ -13,7 +13,52 @@ export default function spectate_init(root, state, channel, users) {
 }
 
 class Spectate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.channel = props.channel;
+    this.users = props.users;
+    this.serverState = props.serverState;
+    this.state = {
+      old_stocks_price: [],
+      stocks_names: ["AMZN", "APPL", "BABA", "CSCO", "FB", "GOOG", "GPRO", "IBM", "INTC", "MSFT", "NVDA", "ORCL", "SNAP", "TSLA", "VZ"],
+      stocks_price: new Array(15).fill(0),
+      stocks_qty: this.serverState.stocks_qty,
+      player1: this.serverState.player1,
+      player2: this.serverState.player2,
+    }
+  }
+
+  componentDidMount() {
+    //Place socket.on call here
+    this.channel.on('get_stocks', (response) => {
+      this.setState({old_stocks_price: this.state.stocks_price})
+      this.setState({stocks_price: response.stocks})
+    });
+
+    this.channel.on("transaction", (response) => {
+      let player1 = response.player1
+      let player2 = response.player2
+      let stocks_qty = response.stocks_qty
+      this.setState({player1: player1, player2: player2, stocks_qty: stocks_qty})
+    });
+  }
+
   render() {
-    return(<div>YOU ARE A SPECTATOR</div>)
+    return(<div>
+      <div className="title-grid">
+        <div className="win-status">Player 1 : ${this.state.player1.wallet}</div>
+        <div className="wallet-status">Player 2 : ${this.state.player2.wallet}</div>
+        <Timer channel={this.channel}/>
+      </div>
+      <div className="padding"></div>
+      <div className="grid">
+        <Player1 playerNumber={this.props.playerNumber} player={this.state.player1} stocksPrice={this.state.stocks_price} />
+        <StocksDB stocksNames={this.state.stocks_names} stocksPrice={this.state.stocks_price} stocksQty={this.state.stocks_qty} stocksOldPrice={this.state.old_stocks_price}/>
+        <Player2 playerNumber={this.props.playerNumber} player={this.state.player2} stocksPrice={this.state.stocks_price} />
+        <Graph channel={this.channel} />
+        <Trending stocksNames={this.state.stocks_names} stocksPrice={this.state.stocks_price} stocksQty={this.state.stocks_qty} stocksOldPrice={this.state.old_stocks_price}/>
+        <Chat channel={this.channel} users={this.users}/>
+      </div>
+    </div>);
   }
 }
