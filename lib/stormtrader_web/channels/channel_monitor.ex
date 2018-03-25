0212256afd5d782cc31f341a6bf6cfc380ebad59
MSP95 <patilmanish1910@gmail.com>
2018-03-25 13:42:08 -0400

    "time update"

--- lib/stormtrader_web/channels/channel_monitor.ex
+++ lib/stormtrader_web/channels/channel_monitor.ex
@@ -47,7 +47,7 @@ defmodule StormtraderWeb.ChannelMonitor do
     {:reply, state, state}
   end
   def handle_call({:send_stocks}, _from, state) do
-    Process.send_after(self(), {:generate_stocks}, 3000) # In 2 hours
+    Process.send_after(self(), {:generate_stocks}, 7000) # In 2 hours
     {:reply, state, state}
   end
   def handle_info({:generate_stocks}, state) do
@@ -58,7 +58,7 @@ defmodule StormtraderWeb.ChannelMonitor do
     end)
 
     state = Map.replace!(state, :stock_price, Enum.take_random(50..1000, 15))
-    Process.send_after(self(), {:generate_stocks}, 3000)
+    Process.send_after(self(), {:generate_stocks}, 7000)
     {:noreply, state}
   end
   # ////////////////////////////////////////////////////////////////////////////
