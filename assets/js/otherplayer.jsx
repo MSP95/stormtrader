f5b122f530d8fbbb441f66cadec8537015f529f1
Varad Choudhari <varad.choudhari@gmail.com>
2018-03-25 19:51:24 -0400

    Fixed UI

--- assets/js/otherplayer.jsx
+++ assets/js/otherplayer.jsx
@@ -16,7 +16,7 @@ export default class OtherPlayer extends React.Component {
     <div className="otherplayer-block">
     <table className="otherplayer-table">
       <tbody>
-      <tr className="spaceunder"><td className="tdspace">Name</td><td className="tdspace">Qty.</td><td className="tdspace">Price</td></tr>
+      <tr className="table-head"><td className="tdspace">Name</td><td className="tdspace">Qty.</td><td className="tdspace">Price</td></tr>
       {this.props.player.own.map((data) => {
         return(<tr className="spaceunder" key={data.id}><td className="tdspace">{data.stock_name}</td><td className="tdspace">{data.qty}</td><td className="tdspace">${data.bought_at}</td></tr>)
       })}
